b4499caeeb8a7bfbd4946a29f5019839a856aa12
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-21 12:15:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Publication publication1 = new Publication();
+            publication1.PublicationYear = 2002;
+            publication1.Author_surname = "Фамилия";
+            publication1.Title = "Публикация";
+            Console.WriteLine(publication1);
+
+            Publication publication2 = new Publication("Внимание", "Шичкин", 2023);
+            Console.WriteLine(publication2);
+            int from = 2000;
+            int to = 2010;
+            Console.WriteLine($"Публикация с годом издания {publication2.PublicationYear} входит в диапазон от {from} до {to}? " +
+                publication2.IsPublicationYearInRange(from, to));
+            publication2.ChangePublicationYear(-15);
+            Console.Write($"Публикация с годом издания {publication2.PublicationYear} входит в диапазон от {from} до {to}? " +
+                publication2.IsPublicationYearInRange(from, to));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Publication.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25
+{
+    internal class Publication
+    {
+        string title;
+        string author_surname;
+        int publicationYear;
+
+        public string Title {
+            get { 
+                if(title == null || title == "")
+                {
+                    return "неизвестный";
+                }
+                return title;
+            } 
+            set { title = value; }
+        }
+        public string Author_surname { 
+            get {
+                if (author_surname == null || author_surname == "")
+                {
+                    return "неизвестный";
+                }
+                return author_surname;
+            } 
+            set { author_surname = value; }
+        }
+        public int PublicationYear {
+            get {
+                return publicationYear;
+            }
+            set { 
+                if(publicationYear < 1990 && publicationYear != 0)
+                {
+                    publicationYear = 1990;
+                }
+                else if(publicationYear > 2024)
+                {
+                    publicationYear = 2024;
+                }
+                else
+                {
+                    publicationYear = value;
+                }
+            }
+        }
+
+        public Publication() { }
+
+        public Publication(string title, string author_surname, int publicationYear)
+        {
+            Title = title;
+            Author_surname = author_surname;
+            PublicationYear = publicationYear;
+        }
+
+        public void ChangePublicationYear(int addedYears)
+        {
+            PublicationYear += addedYears;
+        }
+
+        public bool IsPublicationYearInRange(int from, int to)
+        {
+            return (PublicationYear >= from && PublicationYear <= to);
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {Title}\tФамилия автора: {Author_surname}\tГод издания: {PublicationYear}";
+        }
+    }
+}
